733b02835bfe3445ae3afd50018594b70cc0cde9
Noah Mckibben <180349465+noah-mckibben@users.noreply.github.com>
2025-03-22 16:49:29 -0400

    Structure change

--- cypress/e2e/home.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Home Page', () => {
-  it('should display the welcome message', () => {
-    cy.visit('/src/app/app.component.html');
-    cy.contains('Welcome to Your Angular App').should('be.visible');
-  });
-});
